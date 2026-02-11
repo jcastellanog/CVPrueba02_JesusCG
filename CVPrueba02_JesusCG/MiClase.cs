228eaa1ba0558003a8ee1dcae0deb73a13d4605b
jcastellanog <jesus.castellano@hotmail.com>
2026-02-11 08:44:33 +0100

    Preparación de Ficheros para el Proyecto

---
+++ CVPrueba02_JesusCG/MiClase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVPrueba02_JesusCG
+{
+    public class MiClase
+    {
+    }
+}
